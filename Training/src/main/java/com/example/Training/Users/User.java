f00c4ce589bf04f6f8e405bd657b0e3b2d0fbbce
omarsayed202 <omarsayedabdelmohasen@gmail.com>
2024-08-25 17:26:37 +0300

    add user and post 2

---
+++ Training/src/main/java/com/example/Training/Users/User.java
@@ -0,0 +1,34 @@
+package com.example.Training.Users;
+
+import java.util.Date;
+
+public class User {
+
+    private int id;
+    private String Name;
+    private Date Birthdate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Date getBirthdate() {
+        return Birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        Birthdate = birthdate;
+    }
+}
